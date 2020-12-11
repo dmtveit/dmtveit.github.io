0e2eb430733cf1e241b8e09025083dfa74cefb42
dmtveit <49729874+dmtveit@users.noreply.github.com>
2020-12-11 18:37:35 +0100

    animation testing

--- js/header.js
+++ js/header.js
@@ -5,15 +5,19 @@ window.onscroll = function() {
     if ((currentScrollY > 0) && (prevScrollY === 0)) {
         var stickyHeaderHeight = getComputedStyle(document.documentElement).getPropertyValue("--height-sticky-header");
         var bottomFooterHeight = getComputedStyle(document.documentElement).getPropertyValue("--height-top-header");
-        document.getElementById("top-header").style.height = 0;
-        document.getElementById("main-container").style.paddingTop = stickyHeaderHeight;
-        document.getElementById("bottom-footer").style.height = bottomFooterHeight;
+        //document.getElementById("top-header").style.height = 0;
+        //document.getElementById("main-container").style.paddingTop = stickyHeaderHeight;
+        //document.getElementById("bottom-footer").style.height = bottomFooterHeight;
+
+        document.getElementsByClassName("page").style.transform = "scale(0)";
     } else if ((currentScrollY === 0) && (prevScrollY > 0)) {
         var topHeaderHeight = getComputedStyle(document.documentElement).getPropertyValue("--height-top-header");
         var totalHeaderHeight = getComputedStyle(document.documentElement).getPropertyValue("--height-total-header");
-        document.getElementById("top-header").style.height = topHeaderHeight;
-        document.getElementById("main-container").style.paddingTop = totalHeaderHeight;
-        document.getElementById("bottom-footer").style.height = 0;
+        //document.getElementById("top-header").style.height = topHeaderHeight;
+        //document.getElementById("main-container").style.paddingTop = totalHeaderHeight;
+        //document.getElementById("bottom-footer").style.height = 0;
+
+        document.getElementsByClassName("page").style.transform = "scale(1)";
     }
     prevScrollY = currentScrollY;
 }
