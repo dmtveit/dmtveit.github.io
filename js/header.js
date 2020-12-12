2251810d91767a743cb5f39472513c99c6de428e
dmtveit <49729874+dmtveit@users.noreply.github.com>
2020-12-12 18:08:56 +0100

    animation testing

--- js/header.js
+++ js/header.js
@@ -1,25 +1,32 @@
-/*var prevScrollY = window.scrollY;
+var prevScrollY = window.scrollY;
 
 window.onscroll = function() {
     var currentScrollY = window.scrollY;
     if ((currentScrollY > 0) && (prevScrollY === 0)) {
-        var stickyHeaderHeight = getComputedStyle(document.documentElement).getPropertyValue("--height-sticky-header");
-        var bottomFooterHeight = getComputedStyle(document.documentElement).getPropertyValue("--height-top-header");
-        document.getElementById("top-header").style.height = 0;
-        document.getElementById("main-container").style.paddingTop = stickyHeaderHeight;
-        document.getElementById("bottom-footer").style.height = bottomFooterHeight;
+        //var stickyHeaderHeight = getComputedStyle(document.documentElement).getPropertyValue("--height-sticky-header");
+        //var bottomFooterHeight = getComputedStyle(document.documentElement).getPropertyValue("--height-top-header");
+        //document.getElementById("top-header").style.height = 0;
+        //document.getElementById("main-container").style.paddingTop = stickyHeaderHeight;
+        //document.getElementById("bottom-footer").style.height = bottomFooterHeight;
 
-        //document.getElementsByTagName("header")[0].style.transform = "translate(0, -" + stickyHeaderHeight.toString() + ")";
+        var topHeaderHeight = getComputedStyle(document.documentElement).getPropertyValue("--height-top-header");
+        document.getElementsByTagName("header")[0].style.transform = "translate(0, calc(-1 * " + topHeaderHeight + "))";
         //document.getElementsByTagName("main")[0].style.transform = "translate(0, -" + stickyHeaderHeight.toString() + ")";
     } else if ((currentScrollY === 0) && (prevScrollY > 0)) {
-        var topHeaderHeight = getComputedStyle(document.documentElement).getPropertyValue("--height-top-header");
-        var totalHeaderHeight = getComputedStyle(document.documentElement).getPropertyValue("--height-total-header");
-        document.getElementById("top-header").style.height = topHeaderHeight;
-        document.getElementById("main-container").style.paddingTop = totalHeaderHeight;
-        document.getElementById("bottom-footer").style.height = 0;
+        //var topHeaderHeight = getComputedStyle(document.documentElement).getPropertyValue("--height-top-header");
+        //var totalHeaderHeight = getComputedStyle(document.documentElement).getPropertyValue("--height-total-header");
+        //document.getElementById("top-header").style.height = topHeaderHeight;
+        //document.getElementById("main-container").style.paddingTop = totalHeaderHeight;
+        //document.getElementById("bottom-footer").style.height = 0;
 
-        //document.getElementsByTagName("header")[0].style.transform = "translate(0, 0)";
+        document.getElementsByTagName("header")[0].style.transform = "translate(0, 0)";
         //document.getElementsByTagName("main")[0].style.transform = "translate(0, 0)";
     }
     prevScrollY = currentScrollY;
-}*/
+}
+
+var header = document.getElementsByTagName("header")[0];
+
+header.addEventListener("transitionend", function() {
+    header.style.top = 0;
+});
