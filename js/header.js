6e35390e39731ce7a4363ccb9709f5aadec4db69
dmtveit <49729874+dmtveit@users.noreply.github.com>
2020-12-12 18:59:00 +0100

    animation testing

--- js/header.js
+++ js/header.js
@@ -12,7 +12,7 @@ window.onscroll = function() {
         var topHeaderHeight = getComputedStyle(document.documentElement).getPropertyValue("--height-top-header");
         document.getElementsByTagName("header")[0].style.transform = "translate(0, calc(-1 * " + topHeaderHeight + "))";
         document.getElementsByTagName("main")[0].style.transform = "translate(0, calc(-1 * " + topHeaderHeight + "))";
-        document.getElementById("footer").style.transform = "translate(0, calc(-1 * " + topHeaderHeight + "))";
+        document.getElementById("footer-bar").style.transform = "translate(0, calc(-1 * " + topHeaderHeight + "))";
     } else if ((currentScrollY === 0) && (prevScrollY > 0)) {
         //var topHeaderHeight = getComputedStyle(document.documentElement).getPropertyValue("--height-top-header");
         //var totalHeaderHeight = getComputedStyle(document.documentElement).getPropertyValue("--height-total-header");
@@ -22,7 +22,7 @@ window.onscroll = function() {
 
         document.getElementsByTagName("header")[0].style.transform = "translate(0, 0)";
         document.getElementsByTagName("main")[0].style.transform = "translate(0, 0)";
-        document.getElementById("footer").style.transform = "translate(0, 0)";
+        document.getElementById("footer-bar").style.transform = "translate(0, 0)";
     }
     prevScrollY = currentScrollY;
 }
