21368297b8ada33f6352cf36d102660a697673a1
dmtveit <49729874+dmtveit@users.noreply.github.com>
2020-12-13 17:23:23 +0100

    download scroll testing

--- js/header.js
+++ js/header.js
@@ -1,28 +1,28 @@
 var prevScrollY = window.scrollY;
+var topHeaderHeight = getComputedStyle(document.documentElement).getPropertyValue("--height-top-header");
 
 window.onscroll = function() {
     var currentScrollY = window.scrollY;
-    if ((currentScrollY > 0) && (prevScrollY === 0)) {
-        //var stickyHeaderHeight = getComputedStyle(document.documentElement).getPropertyValue("--height-sticky-header");
-        //var bottomFooterHeight = getComputedStyle(document.documentElement).getPropertyValue("--height-top-header");
-        //document.getElementById("top-header").style.height = 0;
-        //document.getElementById("main-container").style.paddingTop = stickyHeaderHeight;
-        //document.getElementById("bottom-footer").style.height = bottomFooterHeight;
 
-        var topHeaderHeight = getComputedStyle(document.documentElement).getPropertyValue("--height-top-header");
+    // hide header if page is scrolled
+    if ((currentScrollY > 0) && (prevScrollY === 0)) {
+        
         document.getElementsByTagName("header")[0].style.transform = "translate(0, calc(-1 * " + topHeaderHeight + "))";
         document.getElementsByTagName("main")[0].style.transform = "translate(0, calc(-1 * " + topHeaderHeight + "))";
         document.getElementById("footer-bar").style.transform = "translate(0, calc(-1 * " + topHeaderHeight + "))";
+
     } else if ((currentScrollY === 0) && (prevScrollY > 0)) {
-        //var topHeaderHeight = getComputedStyle(document.documentElement).getPropertyValue("--height-top-header");
-        //var totalHeaderHeight = getComputedStyle(document.documentElement).getPropertyValue("--height-total-header");
-        //document.getElementById("top-header").style.height = topHeaderHeight;
-        //document.getElementById("main-container").style.paddingTop = totalHeaderHeight;
-        //document.getElementById("bottom-footer").style.height = 0;
 
         document.getElementsByTagName("header")[0].style.transform = "translate(0, 0)";
         document.getElementsByTagName("main")[0].style.transform = "translate(0, 0)";
         document.getElementById("footer-bar").style.transform = "translate(0, 0)";
+
     }
+
+    // scroll download button if on résumé page
+    if (document.URL.includes("resume")) {
+        document.documentElement.style.setProperty("--download-top", currentScrollY);
+    }
+
     prevScrollY = currentScrollY;
 }
